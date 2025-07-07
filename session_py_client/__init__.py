8324b419328223292321d8404233841b0579738b
Grégoire Compagnon <obeone@obeone.org>
2025-07-08 00:06:01 +0200

    feat(py_utils): add utility functions and validations

---
+++ session_py_client/__init__.py
@@ -0,0 +1,30 @@
+from .utils import (
+    Uint8ArrayToHex,
+    hexToUint8Array,
+    concatUInt8Array,
+    removePrefixIfNeeded,
+    isHex,
+    Uint8ArrayToBase64,
+    base64ToUint8Array,
+    Deferred,
+    checkStorage,
+    checkNetwork,
+    SessionValidationError,
+    SessionValidationErrorCode,
+)
+
+__all__ = [
+    "Uint8ArrayToHex",
+    "hexToUint8Array",
+    "concatUInt8Array",
+    "removePrefixIfNeeded",
+    "isHex",
+    "Uint8ArrayToBase64",
+    "base64ToUint8Array",
+    "Deferred",
+    "checkStorage",
+    "checkNetwork",
+    "SessionValidationError",
+    "SessionValidationErrorCode",
+]
+
